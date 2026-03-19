6d0c5d71c6d4cfb4565672e65474c36745ea6b35
draikon222 <draikon27@gmail.com>
2026-03-19 18:35:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,12 @@
 const { Telegraf, Markup } = require('telegraf');
 
-// TOKEN PUS DIRECT (Fara spatii, fara erori)
 const bot = new Telegraf('8561401872:AAF-s8kvSzpPCBGuybhKwkXQRwt-_bemuXI');
 
 bot.start((ctx) => {
-    ctx.replyWithMarkdown('🛡️ **SENTINEL CORE V1.0**\n[ONLINE]', Markup.inlineKeyboard([
+    ctx.replyWithMarkdown('🛡️ **SENTINEL CORE V1.0**\n[SYSTEM ONLINE]', Markup.inlineKeyboard([
         [Markup.button.callback('🚀 START SNIPER', 'start')],
-        [Markup.button.callback('💰 WALLET', 'wallet')]
+        [Markup.button.callback('💰 MY WALLET', 'wallet')]
     ]));
 });
 
-// Pornire cu log de confirmare
-bot.launch()
-  .then(() => console.log(">>> BOTUL ESTE PORNIT SI FUNCTIONAL! <<<"))
-  .catch((err) => console.log("EROARE START:", err.message));
-
-// Evitam inchiderea botului
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+bot.launch().then(() => console.log(">>> BOTUL ESTE ACTIV <<<"));
